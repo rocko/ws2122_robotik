116338644737afb28740e77801dd1d8b681ecf6d
cka <c_k_a@t-online.de>
2021-10-16 19:42:57 +0200

    x

--- cka/cka/zigzag.py
+++ cka/cka/zigzag.py
@@ -14,10 +14,10 @@ from time import sleep
 import numpy as np
 
 # max Angular and Linear velocities
-BURGER_MAX_LIN_VEL = 0.22
+BURGER_MAX_LIN_VEL = .22
 BURGER_MAX_ANG_VEL = 2.84
-LIN_VEL_STEP_SIZE = 0.01
-ANG_VEL_STEP_SIZE = 0.1
+LIN_VEL_STEP_SIZE = .01
+ANG_VEL_STEP_SIZE = .1
 
 class zigzag(Node):
 	def __init__(self) -> None:
@@ -26,11 +26,11 @@ class zigzag(Node):
 		qos = QoSProfile(depth=10)
 
 		# Initialise variables
-		self.current_pose = [0.0, 0.0, 0.0]
-		self.previous_pose = [0.0, 0.0, 0.0]
-		self.last_pose = [0.0, 0.0, 0.0]
+		self.current_pose = [.0, .0, .0]
+		self.previous_pose = [.0, .0, .0]
+		self.last_pose = [.0, .0, .0]
 
-		self.current_velocity = [0.0, 0.0]
+		self.current_velocity = [.0, .0]
 		
 		self.state = 0
 		# 0 - Get new direction
@@ -38,7 +38,7 @@ class zigzag(Node):
 		# 2 - Turning left
 		# 3 - Turning right
 
-		self.scan_ranges = []
+		self.scan_ranges = [.0, .0, .0]
 
 		self.init_scan_state = False
 		self.init_odom_state = False
@@ -96,13 +96,13 @@ class zigzag(Node):
 			
 			# Statemachine
 			if self.state == 0:  # Is looking for new direction ?
-				if self.scan_ranges[0] > 0.7:  # Check if obstacle in front
+				if self.scan_ranges[0] > .7:  # Check if obstacle in front
 					# Noting in front
-					if self.scan_ranges[1] < 0.6:  # Check if obstacle to the left
+					if self.scan_ranges[1] < .6:  # Check if obstacle to the left
 						self.previous_pose = self.current_pose
 						# Turn right
 						self.state = 3
-					elif self.scan_ranges[2] < 0.6:  # Check if obstacle to the right
+					elif self.scan_ranges[2] < .6:  # Check if obstacle to the right
 						self.previous_pose = self.current_pose
 						# Turn left
 						self.state = 2
@@ -110,25 +110,25 @@ class zigzag(Node):
 						# Drive forwards
 						self.state = 1
 
-				if self.scan_ranges[0] < 0.7: 	# Check if obstacle in front
+				if self.scan_ranges[0] < .7: 	# Check if obstacle in front
 					# Something in front
 					self.previous_pose = self.current_pose					
 					# Turn right
 					self.state = 3
 	
 			elif self.state == 1:  # Drive forwards
-				self.update_cmd_vel(0.3, 0.0)
+				self.update_cmd_vel(.3, .0)
 
 			elif self.state == 2:  # Turn Left State
 				if math.abs(self.previous_pose[2] - self.current_pose[2]) >= 0.5:  # Check if robot has turned away enough # 30 degrees rad
 					self.state = 0
 				else:
-					self.update_cmd_vel(0.0, -0.3)  # Keep turning
+					self.update_cmd_vel(.0, -.3)  # Keep turning
 			elif self.state == 3:  # Turn Right State
-				if math.abs(self.previous_pose[2] - self.current_pose[2]) >= 0.5:
+				if math.abs(self.previous_pose[2] - self.current_pose[2]) >= .5:
 					self.state = 0
 				else:
-					self.update_cmd_vel(0.0, 0.3)
+					self.update_cmd_vel(.0, .3)
 			else:
 				self.state = 0
 
