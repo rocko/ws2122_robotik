92c6cea014fa72b9fa775667526e1ba2b83ad17d
cka <c_k_a@t-online.de>
2021-10-18 12:50:38 +0200

    x

---
+++ cka_biped/setup.py
@@ -0,0 +1,29 @@
+import os
+from glob import glob
+from setuptools import setup
+
+package_name = 'cka_biped'
+
+setup(
+    name=package_name,
+    version='0.0.1',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+	    (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
+	    (os.path.join('share', package_name, 'config'), glob('config/*.yaml')),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='local',
+    maintainer_email='ch.adolph@ostfalia.de',
+    description='Turtlebot 3 - WS2122',
+    license='No license',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+		    'biped_test = cka_biped.biped_test:main'
+        ],
+    },
+)
